83ce0ee41560062d71757a222c33dea38288610f
bala-amypo <bala@amypo.in>
2025-12-17 06:28:04 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -4,12 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import java.time.LocalDate;
 @Entity
 public class Student {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+   
     private Long id;
     private String name;
     private String email;
@@ -18,48 +18,42 @@ public class Student {
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public LocalDate getDob() {
         return dob;
     }
-
     public void setDob(LocalDate dob) {
         this.dob = dob;
     }
-
     public float getCgpa() {
         return cgpa;
     }
-
     public void setCgpa(float cgpa) {
         this.cgpa = cgpa;
     }
-}
-public Student(Long id ,String name,String email,LocalDate dob,float cgpa){
-    this.id=id;
-    this.name=name;
-    this.email=email;
-    this.dob=dob;
-    this.cgpa=cgpa;
+    public Student() {
+    }
+    public Student(Long id, String name, String email, LocalDate dob, float cgpa) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.cgpa = cgpa;
+    } 
+    
     
 }
